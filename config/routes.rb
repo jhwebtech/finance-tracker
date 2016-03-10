0923e73b5b9ae15c5c86731608b7372bb1042fbb
jhwebtech <julieh71@ymail.com>
2016-03-10 15:21:17 -0600

    Add destroy friendship and show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   devise_for :users, :controllers => { :registrations => "user/registrations" }
   resources :user_stocks, except: [:show, :edit, :update]
-  resources :user, only: [:show]
+  resources :users, only: [:show]
   resources :friendships
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
   get 'my_portfolio', to: "users#my_portfolio", as: 'my_portfolio'
+
   get 'search_stocks', to: "stocks#search"
   get 'my_friends' => "users#my_friends"
   get 'search_friends', to: "users#search"
